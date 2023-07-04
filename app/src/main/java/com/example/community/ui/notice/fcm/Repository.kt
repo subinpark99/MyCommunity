5102ca7761b275bdba40aa11bd5b4aea50e335c4
subinpark99 <psb8909@naver.com>
2023-07-04 17:43:10 +0900

    api 키 숨김

--- app/src/main/java/com/example/community/ui/notice/fcm/Repository.kt
+++ app/src/main/java/com/example/community/ui/notice/fcm/Repository.kt
@@ -1,9 +1,11 @@
 package com.example.community.ui.notice.fcm
 
+import com.example.community.R
+
 class Repository {
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAKhIIzwI:APA91bFKOzOjzBcYrUphEJmjphk1zt9Dw4vAJz7xemYPnLF2tGcJ9gSGk_tmpf1FgZXNeSi9HzgjpWvkPcxrFWWbNg8Z5DCaOOiSKl6idHaDa6pngPgWRmd6Gj69jjv9S6pWk2yksEUc"
+        const val SERVER_KEY = "${R.string.server_key}"
         const val CONTENT_TYPE = "application/json"
     }
 }
