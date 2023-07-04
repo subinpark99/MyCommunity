8c0d21ac02bd7c2b149e5dcc3f3f7f895b04fbae
subinpark99 <psb8909@naver.com>
2023-07-04 18:30:45 +0900

    api 키 숨김

--- app/src/main/java/com/example/community/ui/notice/fcm/Repository.kt
+++ app/src/main/java/com/example/community/ui/notice/fcm/Repository.kt
@@ -1,11 +1,12 @@
 package com.example.community.ui.notice.fcm
 
-import com.example.community.R
+import com.example.community.BuildConfig
+
 
 class Repository {
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "${R.string.server_key}"
+        const val SERVER_KEY = BuildConfig.FCM_SERVER_KEY
         const val CONTENT_TYPE = "application/json"
     }
 }
